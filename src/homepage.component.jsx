5336bbebea3473918ce586baefc24b65c361f339
Maximilian Dollinger <max.dollinger@protonmail.com>
2021-01-04 17:54:46 +0100

    first layout steps

---
+++ src/homepage.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+export const HomePage = () => 
+     <div className='homepage'>
+          <div className="directory-menu">
+               <div className="menu-item">
+                    <div className="content">
+                         <h1 className="title">Hats</h1>
+                         <span className="subtitle">SHOP NOW</span>
+                    </div>
+               </div>
+               <div className="menu-item">
+                    <div className="content">
+                         <h1 className="title">Jackets</h1>
+                         <span className="subtitle">SHOP NOW</span>
+                    </div>
+               </div>
+               <div className="menu-item">
+                    <div className="content">
+                         <h1 className="title">Shoe's</h1>
+                         <span className="subtitle">SHOP NOW</span>
+                    </div>
+               </div>
+               <div className="menu-item">
+                    <div className="content">
+                         <h1 className="title">Women's</h1>
+                         <span className="subtitle">SHOP NOW</span>
+                    </div>
+               </div>
+               <div className="menu-item">
+                    <div className="content">
+                         <h1 className="title">Men's</h1>
+                         <span className="subtitle">SHOP NOW</span>
+                    </div>
+               </div>
+          </div>
+     </div>
