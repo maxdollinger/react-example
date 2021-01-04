d8743733005915645aba084517a9a372187cb352
Maximilian Dollinger <max.dollinger@protonmail.com>
2021-01-04 21:01:49 +0100

    shop prieview added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
-import { HomePage } from './pages/homepage/homepage.component';
+import { HomePage } from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+
 import './App.css';
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import PreviewCollection from "../../components/prieview-collection/prieview-collection";
+import shopData from "../../SHOPING_DATA";
+
+const ShopPage = (props) => {
+  const [collections] = useState(shopData);
+
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherProps }) => (
+        <PreviewCollection key={id} {...otherProps}></PreviewCollection>
+      ))}
+    </div>
+  );
+};
+
+export default ShopPage;
