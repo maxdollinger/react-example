9a3658b98083dadc6599804399dbfc5d133c2fc6
Maximilian Dollinger <max.dollinger@protonmail.com>
2021-01-12 20:27:33 +0100

    removed logger from production

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,9 @@ import logger from 'redux-logger';
 
 import rootReducer from './rootReducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+process.env.NODE_ENV === 'development' && (middlewares.push(logger));
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
